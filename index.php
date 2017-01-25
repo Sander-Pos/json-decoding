a46591158551f8112a3578c3f038f6fd89f94ea4
Sander-Pos <sander-62@hotmail.com>
2017-01-25 11:39:48 +0100

    First commit, nothing special.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$json = fopen("stof.json","r");
+$string = fread($json,filesize("stof.json"));
+$stof = array();
+
+//convert json stream to an array
+$stof = json_decode($string,true);
+
+//var_dump(json_decode($string));
+
+//print json data array
+var_dump($stof);
+
+?>
